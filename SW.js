7485a062142953eee10efd422d00da18ff2d36b2
mh10445 <hussein@blockgemini.com>
2019-11-18 20:31:47 +0400

    MKM

--- SW.js
+++ SW.js
@@ -1,1 +1,25 @@
-console.log("SW FILE");
+console.log("SERVICE WORKER");
+
+let version = 1;
+
+self.addEventListener("install", function(e) {
+  console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(handleActivate());
+});
+
+async function handleActivate() {
+  await clients.claim();
+  console.log("Service Worker Activated ");
+}
+
+async function main() {
+  console.log("Service Worker " + version + " Started");
+}
+
+main().catch(err => {
+  console.log(err);
+});
